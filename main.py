65f2d646f454902ebcd6c9b5158c7f7d5795ca64
marsvo1ta <euromobilco@gmail.com>
2025-04-15 13:02:15 +0300

    change host

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def lifespan(app: FastAPI):
     await database.disconnect()
 
 
-app = FastAPI(lifespan=lifespan, root_path="/api")
+app = FastAPI(lifespan=lifespan)
 
 # Підключення статики для SQLAdmin
 app.mount(
